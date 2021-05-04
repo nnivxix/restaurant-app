62cf397c53aae3340d61e5e798573ffe561f418b
nnivxix <adhe.hanasa@gmail.com>
2021-05-04 12:50:54 +0700

    add responsive navbar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,17 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 
-console.log('Hello Coders! :)');
+const btnDrawer = document.querySelector('.drawer')
+const menu = document.querySelector('.desktop ul');
+const myBody = document.body;
+
+btnDrawer.addEventListener('click', e => {
+	menu.classList.toggle('open');
+	e.stopPropagation()
+	console.log('masuk btnDrawer')
+})
+myBody.addEventListener('click', e => {
+	menu.classList.remove('open');
+	e.stopPropagation()
+})
+console.log(myBody)
